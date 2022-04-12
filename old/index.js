934a7a06e5d5c5a49020ad7547b7f0cfb3649aa1
es5es5 <es5e5s@nate.com>
2022-04-12 15:43:42 +0900

    index.js

---
+++ old/index.js
@@ -0,0 +1,6 @@
+import a_hello from './a.js'
+import b_hello from './b.js'
+import c_hello from './c.js'
+console.log('a_hello', a_hello)
+console.log('b_hello', b_hello)
+console.log('c_hello', c_hello)
